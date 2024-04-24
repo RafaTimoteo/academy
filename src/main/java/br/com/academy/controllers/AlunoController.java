1c68e8c5f40adcda470a2bcde10374a933bd917b
Rafael Timoteo <rafaeltimoteolima1202@gmail.com>
2024-04-24 14:01:00 -0300

    Criação dos metodos para deletar e editar alunos

--- src/main/java/br/com/academy/controllers/AlunoController.java
+++ src/main/java/br/com/academy/controllers/AlunoController.java
@@ -3,6 +3,7 @@ package br.com.academy.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -40,4 +41,26 @@ public class AlunoController {
         return mv;
     }
     
+    @GetMapping("/alterar/{id}")
+    public ModelAndView alterar(@PathVariable("id") Integer id) {
+        ModelAndView mv = new ModelAndView();
+        Aluno aluno = alunorepositorio.findById(id).orElse(null);
+        mv.addObject("aluno", aluno);
+        mv.setViewName("aluno/alterar");
+        return mv;
+    }
+
+    @PostMapping("alterar")
+    public ModelAndView alterarAluno(Aluno aluno) {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("redirect:/alunos-adicionados");
+        alunorepositorio.save(aluno);
+        return mv;
+    }
+
+    @GetMapping("/deletar/{id}")
+    public String exluirAluno(@PathVariable("id") Integer id) {
+        alunorepositorio.deleteById(id);
+        return "redirect:/alunos-adicionados";
+    }
 }
